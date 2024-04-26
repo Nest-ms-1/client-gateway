7d4290980425bd8999283b9d870e59d4257f15da
Jhonatan Estiven Becerra Pedrozo <jsbpdeveloper@gmail.com>
2024-04-25 20:50:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Client Gateway
+
+## Dev
+
+1. CLonar el repo
+2. Instalar dependencias
+3. Crear un archivo env basado en el example `env.template`
+4. Ejecutar `npm run start:dev`
---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config'
+import * as Joi from 'joi'
+
+interface EnvVars {
+  PORT: number
+  PRODUCTS_MICROSERVICE_HOST: string
+  PRODUCTS_MICROSERVICE_PORT: number
+}
+
+const envsSchema = Joi.object({
+  PORT: Joi.number().required(),
+  PRODUCTS_MICROSERVICE_HOST: Joi.string().required(),
+  PRODUCTS_MICROSERVICE_PORT: Joi.number().required(),
+}).unknown(true)
+
+const { error, value } = envsSchema.validate(process.env)
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`)
+}
+const envsVars: EnvVars = value
+export const envs = {
+  port: envsVars.PORT,
+  productsMicroserviceHost: envsVars.PRODUCTS_MICROSERVICE_HOST,
+  productsMicroservicePort: envsVars.PRODUCTS_MICROSERVICE_PORT,
+}
